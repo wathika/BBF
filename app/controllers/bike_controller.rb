e662e42ac022c3112a625830f077cbbb45a2169a
wathika <vcwathika@gmail.com>
2018-03-14 13:58:34 +0300

    some validation that actually work

--- app/controllers/bike_controller.rb
+++ app/controllers/bike_controller.rb
@@ -9,10 +9,30 @@ class BikeController < ApplicationController
     @bike = Bike.new(bike_params)
 
     respond_to do |format|
+      @errorMotorNo = []
+      @errorBatteryNo = []
+      @errorChargerNo = []
+      @errorColour = []
       if @bike.save
         format.js
+        @resetForm = "1"
       else
-        format.json { render json: @bike.errors.messages, status: :unprocessable_entity }
+        format.js  {render layout: false}
+        @bike.errors.any?
+        #PUSH ERROR MESSAGES INTO ERROR ARRAYS
+        if (@bike.errors["motorNo"] != nil)
+          @errorMotorNo.push(@bike.errors["motorNo"][0])
+        end
+        if (@bike.errors["batteryNo"] != nil)
+          @errorBatteryNo.push(@bike.errors["batteryNo"][0])
+        end
+        if (@bike.errors["chargerNo"] != nil)
+          @errorChargerNo.push(@bike.errors["chargerNo"][0])
+        end
+        if (@bike.errors["colour"] != nil)
+          @errorColour.push(@bike.errors["colour"][0])
+        end
+        @resetForm = "0"
       end
     end
   end
--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,7 @@
 class Bike < ApplicationRecord
   validates_presence_of :motorNo, :batteryNo, :chargerNo, :colour
-  validates_uniqueness_of :motorNo, :batteryNo, :chargerNo
+  validates_uniqueness_of :motorNo, :batteryNo, :chargerNo, message: "Serial number already exists"
+  # validates_format_of :motorNo, with: '', message: "Please enter motor number in the following format: BBM(M)********"
+  # validates_format_of :batteryNo, with: '', message: "Please enter battery number in the following format: BBM(B)********"
+  # validates_format_of :chargerNo, with: '', message: "Please enter motor number in the following format: BBM(M)********"
 end
