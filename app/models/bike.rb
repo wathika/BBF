bd109c81fa2bc6e537b7227e8298bce5bd0de89f
wathika <vcwathika@gmail.com>
2018-03-15 23:13:39 +0300

    validates format of serial numbers

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,9 @@
 class Bike < ApplicationRecord
+  validates_format_of :motorNo, with: /BBF\(M\)N\d+/, message: "Please enter motor number in the following format: BBM(M)N********"
+  validates_format_of :batteryNo, with: /BBF\(B\)N\d+/, message: "Please enter battery number in the following format: BBM(B)N********"
+  validates_format_of :chargerNo, with: /BBF\(C\)N\d+/, message: "Please enter motor number in the following format: BBM(C)N********"
   validates_presence_of :motorNo, :batteryNo, :chargerNo, :colour
   validates_uniqueness_of :motorNo, :batteryNo, :chargerNo, message: "Serial number already exists"
-  # validates_format_of :motorNo, with: '', message: "Please enter motor number in the following format: BBM(M)********"
-  # validates_format_of :batteryNo, with: '', message: "Please enter battery number in the following format: BBM(B)********"
-  # validates_format_of :chargerNo, with: '', message: "Please enter motor number in the following format: BBM(M)********"
+  # validates_format_of :batteryNo, with: '', message: "Please enter battery number in the following format: BBM(B)N********"
+  # validates_format_of :chargerNo, with: '', message: "Please enter motor number in the following format: BBM(M)N********"
 end
