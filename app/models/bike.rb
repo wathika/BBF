c064b9403375373f53fd054a2533dca798118b58
wathika <vcwathika@gmail.com>
2018-03-15 23:19:56 +0300

    removed reduntant comment

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -4,6 +4,3 @@ class Bike < ApplicationRecord
   validates_format_of :chargerNo, with: /BBF\(C\)N\d+/, message: "Please enter motor number in the following format: BBM(C)N********"
   validates_presence_of :motorNo, :batteryNo, :chargerNo, :colour
   validates_uniqueness_of :motorNo, :batteryNo, :chargerNo, message: "Serial number already exists"
-  # validates_format_of :batteryNo, with: '', message: "Please enter battery number in the following format: BBM(B)N********"
-  # validates_format_of :chargerNo, with: '', message: "Please enter motor number in the following format: BBM(M)N********"
-end
