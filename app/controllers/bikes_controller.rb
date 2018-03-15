4be2a63a8ad2213b4f9a3d848745db628af66bb0
wathika <vcwathika@gmail.com>
2018-03-16 02:01:34 +0300

    validates presence of motorNo and Colour

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -7,11 +7,21 @@ class BikesController < ApplicationController
     @bike = Bike.new(bike_params)
 
     respond_to do |format|
+      @errorMotorNo = []
+      @errorColour = []
       if @bike.save
         format.js
         @resetForm = "1"
       else
-        format.js
+        format.js {render layout: false}
+        @bike.errors.any?
+        #PUSH ERROR MESSAGES INTO ERROR ARRAYS
+        if (@bike.errors["motorNo"] != nil)
+          @errorMotorNo.push(@bike.errors["motorNo"][0])
+        end
+        if (@bike.errors["colour"] != nil)
+          @errorColour.push(@bike.errors["colour"][0])
+        end
         @resetForm = "0"
       end
     end
