348fdf1e42e33bb0f50953aeb37ea56a340b4fd0
wathika <vcwathika@gmail.com>
2018-03-16 05:26:39 +0300

    some ui changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
   def index
     @bike = Bike.new
+    @bikes = Bike.all
 
     @battery = Battery.new
+    @batteries = Battery.all
 
     @charger = Charger.new
+    @chargers = Charger.all
   end
 end
